b29cfdd1929c87f529f8187c0725870b5898090c
Shakhboz <shfazliddinov1940@gmail.com>
2024-01-04 19:22:28 +0500

    added perechisleniye and payment type to tr_history

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_04_034241) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_04_140552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -328,6 +328,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_04_034241) do
     t.boolean "price_in_usd", default: false
     t.bigint "user_id"
     t.boolean "first_record", default: false
+    t.integer "payment_type", default: 0
     t.index ["delivery_from_counterparty_id"], name: "index_transaction_histories_on_delivery_from_counterparty_id"
     t.index ["expenditure_id"], name: "index_transaction_histories_on_expenditure_id"
     t.index ["sale_from_local_service_id"], name: "index_transaction_histories_on_sale_from_local_service_id"
