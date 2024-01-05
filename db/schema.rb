c381559c016e90e480a85b2dcd9791a4d5fe0018
Shakhboz <shfazliddinov1940@gmail.com>
2024-01-05 18:24:20 +0500

    added weight to prod category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_05_114308) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_05_131616) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -162,6 +162,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_05_114308) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "weight", default: 0
   end
 
   create_table "product_entries", force: :cascade do |t|
