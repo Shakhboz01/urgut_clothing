b21e9c3cddf673f70f457304b338ff4c5ad7d3be
Shakhboz <shfazliddinov1940@gmail.com>
2024-01-05 17:55:45 +0500

    added improvement

--- app/controllers/delivery_from_counterparties_controller.rb
+++ app/controllers/delivery_from_counterparties_controller.rb
@@ -25,7 +25,7 @@ class DeliveryFromCounterpartiesController < ApplicationController
     @products = Product.active.order(:name)
     if (product_entries = @delivery_from_counterparty.product_entries).exists?
       @price_in_percentage = product_entries.last.price_in_percentage
-      @products = @products.where(product_category_id: product_entries.last.product_category_id).order(:name)
+      @products = @products.where(product_category_id: product_entries.last.product.product_category_id).order(:name)
     end
   end
 
@@ -109,6 +109,6 @@ class DeliveryFromCounterpartiesController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def delivery_from_counterparty_params
-    params.require(:delivery_from_counterparty).permit(:total_price, :status, :total_paid, :payment_type, :comment, :provider_id)
+    params.require(:delivery_from_counterparty).permit(:total_price, :status, :total_paid, :payment_type, :comment, :provider_id, :product_category)
   end
 end
