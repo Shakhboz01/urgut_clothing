46cc692aa6dcc86c5a8ff05ea2643cb664b77c58
Shakhboz <shfazliddinov1940@gmail.com>
2024-01-06 09:18:17 +0500

    added buyers2.json

--- app/interactions/data_population/populate_buyers.rb
+++ app/interactions/data_population/populate_buyers.rb
@@ -1,7 +1,7 @@
 module DataPopulation
   class PopulateBuyers < ActiveInteraction::Base
     def execute
-      buyers_data = JSON.parse(File.read('app/assets/javascripts/buyers.json'))
+      buyers_data = JSON.parse(File.read('app/assets/javascripts/buyers2.json'))
       buyers_data.each do |buyer_data|
         create_buyers(buyer_data)
       end
--- app/interactions/data_population/populate_sellers.rb
+++ app/interactions/data_population/populate_sellers.rb
@@ -17,24 +17,9 @@ module DataPopulation
           active: true
         )
       delivery_from_counterparty = DeliveryFromCounterparty.create!(
-        provider: provider, user_id: User.first.id, price_in_usd: true
+        provider: provider, user_id: User.first.id, price_in_usd: true,
+        total_price: data['in_usd'], status: :closed, total_paid: 0
       )
-      product = Product.find_or_create_by(
-        name: 'TEST0', buy_price: 0.1, sell_price: 0.1, price_in_usd: true, unit: 0, product_category_id: ProductCategory.last.id
-      )
-      ProductEntry.create(
-        paid_in_usd: true,
-        buy_price: 0.1,
-        sell_price: 0.2,
-        delivery_from_counterparty_id: delivery_from_counterparty.id,
-        storage_id: Storage.first.id
-      )
-      delivery_from_counterparty.update(
-        total_price: data['in_usd'],
-        status: :closed,
-        total_paid: 0
-      )
-      product.update(active: false) if product.active
     end
   end
 end
