5ce9f750ded265ad080b9b035c1cdcac00a6b955
Shakhboz <shfazliddinov1940@gmail.com>
2024-01-28 09:52:37 +0500

    added strip for size name

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,10 @@
 class Size < ApplicationRecord
   validates_uniqueness_of :name
+  before_validation :strip_whitespace
+
+  private
+
+  def strip_whitespace
+    self.name = name.strip.split.join.downcase
+  end
 end
