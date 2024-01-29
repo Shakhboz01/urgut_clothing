d4f80f0f6ead360fbf7ba81934b28ff1126816d1
Shakhboz <shfazliddinov1940@gmail.com>
2024-01-29 15:22:18 +0500

    added minor change

--- app/models/pack.rb
+++ app/models/pack.rb
@@ -1,5 +1,4 @@
 class Pack < ApplicationRecord
-  validates_uniqueness_of :name
   has_many :product_size_colors
   has_many :product_entries
 end
--- app/models/product_size_color.rb
+++ app/models/product_size_color.rb
@@ -2,4 +2,5 @@ class ProductSizeColor < ApplicationRecord
   belongs_to :color
   belongs_to :size
   belongs_to :pack
+  validates_presence_of :amount
 end
